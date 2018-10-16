6ef5fec2b8d7906887da13a681f2c8249a0aed83
lukasz19944 <lukasz19944@gmail.com>
2018-10-16 09:17:02 +0200

    Pierwsza wersja gry po ukończeniu kursu

---
+++ Assets/Scripts/BallController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallController : MonoBehaviour {
+
+    // increase speed every paddle touch
+    public float difficultyMultiplier = 1.1f;
+
+    public float minXSpeed = 0.8f;
+    public float maxXSpeed = 1.2f;
+
+    public float minYSpeed = 0.8f;
+    public float maxYSpeed = 1.2f;
+
+    private Rigidbody2D rb;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody2D>();
+        rb.velocity = new Vector2(
+            Random.Range(minXSpeed, maxXSpeed) * (Random.value > 0.5f ? -1 : 1),        // random direction of ball initialization for X axis
+            Random.Range(minYSpeed, maxYSpeed) * (Random.value > 0.5f ? -1 : 1)         // random direction of ball initialization for Y axis
+        );
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter2D(Collider2D collision) {
+        if (collision.CompareTag("Limit")) {
+            rb.velocity = new Vector2(rb.velocity.x, -rb.velocity.y);
+            GetComponent<AudioSource>().Play();
+        } else if (collision.CompareTag("Paddle")) {
+            rb.velocity = new Vector2(-rb.velocity.x * difficultyMultiplier, rb.velocity.y * difficultyMultiplier);
+            GetComponent<AudioSource>().Play();
+        }
+    }
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour {
+
+    public GameObject ballPrefab;
+
+    public Text score1Text;
+    public Text score2Text;
+
+    public float scoreCoords = 3.4f;
+
+    private BallController currentBall;
+
+    private int score1 = 0;
+    private int score2 = 0;
+
+
+	// Use this for initialization
+	void Start () {
+        SpawnBall();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (currentBall != null) {
+            if (currentBall.transform.position.x > scoreCoords) {
+                score1++;
+                Destroy(currentBall.gameObject);
+                SpawnBall();
+            }
+
+            if (currentBall.transform.position.x < -scoreCoords) {
+                score2++;
+                Destroy(currentBall.gameObject);
+                SpawnBall();
+            }
+        }
+	}
+
+    void SpawnBall() {
+        GameObject ball = Instantiate(ballPrefab, transform);
+
+        currentBall = ball.GetComponent<BallController>();
+
+        currentBall.transform.position = Vector3.zero;
+
+        score1Text.text = score1.ToString();
+        score2Text.text = score2.ToString();
+    }
+}
